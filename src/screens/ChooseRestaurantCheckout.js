4908c77c8b6c2987115dd1f08ec4ff3356629682
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-05-16 18:56:02 +0200

    Heading One to ChooseRestaurantCheckout Screen

---
+++ src/screens/ChooseRestaurantCheckout.js
@@ -0,0 +1,13 @@
+import { SafeAreaView, ScrollView } from "react-native-web";
+
+export default function ChooseRestaurantCheckout () {
+    return(
+      <SafeAreaView style={styles.container}>
+          <ScrollView style={styles.scrollview}>
+            <Text style={styles.headingOne} >Choose Kigali</Text>
+          </ScrollView>
+      </SafeAreaView>  
+    )
+
+    
+}
