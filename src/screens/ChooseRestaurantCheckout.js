72d9513ac06b39ed1bb2ffd565c6493af29abf7f
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-05-16 19:26:46 +0200

    Polished Heading one and Heading Two

--- src/screens/ChooseRestaurantCheckout.js
+++ src/screens/ChooseRestaurantCheckout.js
@@ -1,13 +1,37 @@
-import { SafeAreaView, ScrollView } from "react-native-web";
+// import { SafeAreaView, ScrollView, StyleSheet } from "react-native-web";
+import { StyleSheet, Text, SafeAreaView, Dimensions, ScrollView } from 'react-native';
+
+let height = Dimensions.get("window").height;
+let width = Dimensions.get("window").width;
 
 export default function ChooseRestaurantCheckout () {
     return(
       <SafeAreaView style={styles.container}>
           <ScrollView style={styles.scrollview}>
-            <Text style={styles.headingOne} >Choose Kigali</Text>
+            <Text style={styles.headingOne}> Choose Kigali </Text>
+            <Text style={styles.headingTwo}> Drinks </Text>
           </ScrollView>
       </SafeAreaView>  
-    )
-
-    
+    );
 }
+
+const styles = StyleSheet.create({
+    scrollview: {
+        backgroundColor: "white",
+        height: (height * 90 / 100),
+        width,
+        padding: 10,
+    },
+    headingOne: {
+        color: "#434445",
+        fontWeight: "bold",
+        fontSize: 26,
+        textAlign: "right"
+    },
+    headingTwo: {
+        color: "#F7941D",
+        textAlign: "right",
+        fontSize: 27,
+        marginTop: 10,
+    }
+})
