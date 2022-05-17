06f7dcd2fad2494208b7acc34ab8d9255b973551
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-05-17 09:17:46 +0200

    classified victuals info victualImage and victualInfo

--- src/screens/ChooseRestaurantCheckout.js
+++ src/screens/ChooseRestaurantCheckout.js
@@ -17,14 +17,18 @@ export default function ChooseRestaurantCheckout () {
             {/* //TODO: Turn this into a re-usable component */}
 
             <View style={styles.victuals}>
-                <Text style={styles.victualIngredients}> Kalfir Lime Vodka, Lemongrass, Ginger, Citrus </Text>
-                <Text style={styles.victualName}> Tom Yummy - 12.5 </Text>
-                <Text style={styles.victualPrice}> 5000 Frw </Text>
-                <Image style={styles.tinyLogo} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png',}} />
-                <View style={styles.victualAmountAction}> 
-                    <Icon name="minus" style={styles.icon}/>
-                        <Text style={styles.victualAmount}> 2 </Text>
-                    <Icon name="plus" style={styles.icon}/>
+                <View style={styles.victualImage}>
+                    <Image style={styles.tinyLogo} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png',}} />
+                </View>
+                <View style={styles.victualInfo}>
+                    <Text style={styles.victualIngredients}> Kalfir Lime Vodka, Lemongrass, Ginger, Citrus </Text>
+                    <Text style={styles.victualName}> Tom Yummy - 12.5 </Text>
+                    <Text style={styles.victualPrice}> 5000 Frw </Text>
+                    <View style={styles.victualAmountAction}> 
+                        <Icon name="minus" style={styles.icon}/>
+                            <Text style={styles.victualAmount}> 2 </Text>
+                        <Icon name="plus" style={styles.icon}/>
+                    </View>
                 </View>
             </View>
 
@@ -56,7 +60,16 @@ const styles = StyleSheet.create({
         marginTop: 10,
     },
     victuals: {
-        color: "black",
+        marginTop: 14,
+        marginLeft: 10,
+        marginRight: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 5,
+        paddingRight: 5,
+        borderRadius: 10,
+        backgroundColor: "#f0f5fa",
+        maxWidth: 400,
     },
     icon: {
         fontSize: 20,
@@ -66,4 +79,10 @@ const styles = StyleSheet.create({
         width: 50,
         height: 50,
     },
+    victualImage: {
+        maxWidth: 50,
+    },
+    victualInfo: {
+        maxWidth: 350,
+    }
 })
