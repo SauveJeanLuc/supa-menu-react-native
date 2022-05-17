fc2354f5cc382823821ad50bafdaa3466405e98c
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-05-17 15:59:32 +0200

    set ChooseRestaurantCheckout  to App.js for test

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ChooseRestaurantCheckout from './src/screens/ChooseRestaurantCheckout';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <ChooseRestaurantCheckout></ChooseRestaurantCheckout>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(250, 250, 250)',
     alignItems: 'center',
     justifyContent: 'center',
   },
