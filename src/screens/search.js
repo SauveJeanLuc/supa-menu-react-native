ed5355a48f025f4c9293154439a2caa0a0c5ff13
Twizeyimana elissa <twizelissa@gmail.com>
2022-05-20 00:02:11 +0200

    adding icon

--- src/screens/search.js
+++ src/screens/search.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Fontisto, EvilIcons } from "@expo/vector-icons";
 import {
   TouchableOpacity,
   SafeAreaView,
@@ -15,10 +16,19 @@ function Search() {
   return (
     <View style={styles.container}>
       <View style={styles.search}>
+        <EvilIcons name="search" size={30} color="black" />
         <TextInput
           placeholder="Search for your favorite restaurant"
           style={styles.textInput}
         />
+      </View>
+
+      <View>
+        <Text style={styles.or}>Or</Text>
+        <View style={styles.qrcode}>
+          <Fontisto name="qrcode" size={80} color="black" />
+        </View>
+        <Text style={styles.scan}>Scan ,Pay & Enjoy </Text>
       </View>
     </View>
   );
@@ -33,19 +43,42 @@ const styles = StyleSheet.create({
     width: "100%",
   },
   search: {
+    // flexDirection: "row",
     marginTop: "30%",
-  },
-  textInput: {
-    paddingLeft: "15%",
-    backgroundColor: "#fff",
     flexDirection: "row",
+    backgroundColor: "#fff",
     borderWidth: 1.5,
+    paddingLeft: "15%",
     borderColor: "#fff",
     height: 45,
     borderRadius: 20,
-    marginLeft: "5%",
-    marginRight: "5%",
+    marginRight: "10%",
+    marginLeft: "10%",
+  },
+  textInput: {
+    flex: 1,
     fontSize: 13,
     color: "black",
+  },
+
+  or: {
+    marginTop: "15%",
+    fontSize: 18,
+    color: "black",
+    marginLeft: "40%",
+    marginRight: "40%",
+    fontWeight: "bold",
+  },
+  qrcode: {
+    marginTop: "10%",
+    marginLeft: 150,
+    marginRight: 30,
+  },
+  scan: {
+    marginTop: "18%",
+    marginLeft: "30%",
+    fontSize: 18,
+    color: "black",
+    fontWeight: "bold",
   },
 });
