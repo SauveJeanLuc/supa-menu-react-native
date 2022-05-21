462c958b192f173d6f17538a90921657581c0433
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-05-21 20:23:09 +0200

    Refined succpay

--- src/screens/SuccPay.js
+++ src/screens/SuccPay.js
@@ -2,34 +2,43 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 import basketImage from '../assets/pay.png';
 import { AntDesign } from '@expo/vector-icons';
+import SuperMenu from '../assets/SuperMenu.svg'
+import Svg, {Use} from 'react-native-svg';
+
+const appOrange = '#ee8b07';
 
 export default function SuccPay({navigation}) {
   // const {container,banner, highright, title, paragraph, button, buttonContainer, buttonLabel} = styles;
   const {container,imge,words,line,banner,title,paragraph,link, button, buttonContainer, buttonLabel} = styles;
+
   return (
     <View style={container}>
-       <View style={banner}>
-          <Image style={imge} source={basketImage}  />
-       </View>
-      <View>
-      <View style={words}>
-        <Text style={title}>Payment Success</Text>
-        <Text style={paragraph}>we will send order details and emvoice in your conatact no. and registered email</Text>
-       </View>
-       <View style={line}>
-         <Text style={link}>Check Details 
-         <AntDesign name="arrowright" size={24} color="#ee8b07" /></Text>
-       </View>
+
+      <View style={banner}>
+        <Image style={imge} source={basketImage}  />
       </View>
+
+      <View style={words}>
+        <Text style={title}>Payment Success, Yayy!</Text>
+        <Text style={paragraph}>we will send order details and emvoice in your conatact no. and registered email</Text>
+      </View>
+
+      <View style={line}>
+         <Text style={link}>Check Details </Text>
+         <AntDesign name="arrowright" size={24} color="#ee8b07" style={styles.linkArrow}/>
+       </View>
        
-       <View style={buttonContainer}>
-       <Pressable style={button}>
-         <Text style={buttonLabel}>Download Invoice</Text>
-       </Pressable>
-       </View>
-       <View>
-        <Text style={paragraph} onPress={()=>navigation.navigate("RateUs")}>SupaMenu</Text>
-       </View>
+      <View style={buttonContainer}>
+        <Pressable style={button}>
+          <Text style={buttonLabel}>Download Invoice</Text>
+        </Pressable>
+      </View>
+
+      <View onPress={()=>navigation.navigate("RateUs")} style={styles.footerLogo}>
+        <Text style={[styles.footerLogoSupa, styles.footerLogoText]} >Supa</Text>
+        <Text style={[styles.footerLogoMenu, styles.footerLogoText]} >Menu</Text>
+      </View>
+
     </View>
   );
 }
@@ -37,22 +46,28 @@ export default function SuccPay({navigation}) {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'black',
+    flexDirection: 'column',
     flex: 1,
     // justifyContent:'space-between',
     // alignContent:'center',
   },
+
   banner: {
+    flex: 1,
     padding:60,
     height: '40%',
     backgroundColor:'black',
     justifyContent: 'space-around',
     alignItems: 'center'
- },
+  },
+
   imge:{
+
     marginTop:60,
     height:300,
     width:300,
   },
+
   title:{
     fontWeight:'bold',
     color:'#ee8b07',
@@ -64,6 +79,7 @@ const styles = StyleSheet.create({
     fontSize:16
   },
   words:{
+    flex: 1,
     alignItems:'center',
     justifyContent:'space-around',
     textAlign:'center',
@@ -72,22 +88,33 @@ const styles = StyleSheet.create({
     padding:30
   },
   link:{
-    padding:30,
+    flex: 1,
+    fontSize: 20,
     color:'#ee8b07',
     textAlign:'center',
     fontWeight:'700',
+    marginLeft: 100,
+  },
+  linkArrow: {
+    flex: 1,
+    marginTop: 4,
+    fontSize: 23,
   },
   line:{
+    flex:1,
     flexDirection:'row',
+    justifyContent:'space-around',
+    alignItems:'center',
   },
   buttonContainer: {
+    flex: 1,
     width: '100%',
     justifyContent:'space-around',
-    alignItems:'center'
+    alignItems:'center',
   },  
   button: {
     backgroundColor: '#FFA451',
-    width: 250,
+    width: 350,
     display: 'flex',
     padding: 20,
     borderRadius: 10,
@@ -96,5 +123,22 @@ const styles = StyleSheet.create({
   },
   buttonLabel: {
     color: 'white'
+  },
+  footerLogo: {
+    flex: 1,
+    flexDirection:'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: 40,
+  },
+  footerLogoText:{
+    fontSize: 47,
+    fontWeight: '700'
+  },
+  footerLogoSupa: {
+    color: 'white'
+  },
+  footerLogoMenu: {
+    color: appOrange,
   }
 })
