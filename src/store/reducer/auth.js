e4ff0a42a994a7df8e8f6965bd6098b04e836755
Twizeyimana elissa <twizelissa@gmail.com>
2022-06-09 22:21:11 +0200

    signup

---
+++ src/store/reducer/auth.js
@@ -0,0 +1,18 @@
+import { SIGNIN, SIGNUP } from "../actions/auth";
+
+const initialState = {
+  token: null,
+  user: {},
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SIGNIN:
+      return {
+        token: action.token,
+        user: action.user,
+      };
+    default:
+      return state;
+  }
+};
---
+++ src/store/reducer/menu.js
@@ -0,0 +1,15 @@
+import { FETCH_ALL, GETONE } from "../actions/menu";
+
+const initialState = {
+  items: [],
+  item: {},
+};
+
+export default (state = { items: [], item: {} }, action) => {
+  switch (action.type) {
+    case FETCH_ALL:
+      return { ...state, items: action.payload };
+    default:
+      return state;
+  }
+};
